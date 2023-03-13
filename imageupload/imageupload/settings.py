d78336774f741390e5157bb8c5fb45f224c72190
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-03-13 18:32:38 +0100

    feat: add user auth

--- imageupload/imageupload/settings.py
+++ imageupload/imageupload/settings.py
@@ -131,3 +131,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
